05e3c74f24e9ec5c0717d9ba080f8e87953cd09f
Troels Laegsgaard <laegsgaardtroels@gmail.com>
2020-02-04 20:23:48 +0100

    Added agent test. Logging and refactored.

--- chess/game.py
+++ chess/game.py
@@ -1,8 +1,13 @@
 from chess.board import Board
+from chess.agent import Agent
 from chess.pieces import *
+from chess.utils import chess_notation
 
 import copy
+import logging
 from itertools import chain
+
+logger = logging.getLogger(__name__)
 
 
 class Game:
@@ -28,9 +33,10 @@ class Game:
         if len(chess_notation) != 2:
             return None
         letter, number = chess_notation
+        number = int(number)
         if not (letter in Game.letters and number in Game.numbers):
             return None
-        i = 8 - int(number)
+        i = 8 - number
         j = dict(zip(Game.letters, range(8)))[letter]
         return i, j
 
@@ -67,8 +73,7 @@ class Game:
     def is_check(self, color):
         king = self.board.get_king(color)
         enemy_moves = [
-            to
-            for from_, to in self.moves(self.opponent_color())
+            to for from_, to in self.moves(self.opponent_color(color))
         ]
         return king.position in enemy_moves
 
@@ -83,72 +88,64 @@ class Game:
         return False
 
     def cant_move_out_of_check(self, color):
-        king = self.board.get_king(color)
-        for to in king.moves():
-            new_board = self.simulate_move(king.position, to)
-            new_board.current_color = color
+        for move in self.moves(color):
+            new_board = self.simulate_move(*move)
             if not new_board.is_check(color):
                 return False
         return True
 
-    def opponent_color(self):
+    def opponent_color(self, color=None):
         from_to = {'white': 'black', 'black': 'white'}
-        return from_to[self.current_color]
+        if color is None:
+            return from_to[self.current_color]
+        else:
+            return from_to[color]
 
-    def play(self):
-        while not (self.is_checkmate(board.current_color) or self.is_draw()):
+    def play(self, agent=None):
+        while not (self.is_checkmate(self.current_color) or self.is_draw()):
             print(self)
             print()
-            str_from = input('Move from: ')
-            str_to = input('Move to: ')
-            print()
-            from_ = self.translate(str_from)
-            to = self.translate(str_to)
+            if self.current_color == 'black':
+                from_, to = agent.policy(self)
+            else:
+                str_from = input('Move from: ')
+                str_to = input('Move to: ')
+                print()
+                from_ = self.translate(str_from)
+                to = self.translate(str_to)
 
-            if from_ is None or to is None:
-                print("Invalid move...\n")
-                continue
-            if isinstance(self.board[from_], Empty):
-                print("Not a piece...\n")
-                continue
-            if self.board[from_] is None:
-                print("Not on the board...\n")
-                continue
-            if to not in self.board[from_].moves():
-                print("Invalid move...\n")
-                continue
-            if self.board[from_].color != self.board.current_color:
-                print('Not this players turn...\n')
-                continue
-
+                if from_ is None or to is None:
+                    print("Invalid move...\n")
+                    continue
+                if isinstance(self.board[from_], Empty):
+                    print("Not a piece...\n")
+                    continue
+                if self.board[from_] is None:
+                    print("Not on the board...\n")
+                    continue
+                if to not in self.board[from_].moves():
+                    print("Can move this piece to here...\n")
+                    continue
+                if self.board[from_].color != self.current_color:
+                    print('Not this players turn...\n')
+                    continue
             self.move(from_, to)
-            self.current_color = self.opponent_color()
+            logger.info(
+                f"\n\n{self.opponent_color()} moved "
+                f"{' -> '.join(chess_notation((from_, to)))}."
+                f"\n\n{game}\n"
+            )
+        print()
+        print(f"Winner is {self.opponent_color()}")
+        logger.info(f"winner is {self.opponent_color()}.")
 
-    def playout(self):
-        while not (game.is_checkmate(board.current_color) or game.is_draw()):
-            print(self)
-            print()
-            str_from = input('Move from: ')
-            str_to = input('Move to: ')
-            print()
-            from_ = self.translate(str_from)
-            to = self.translate(str_to)
 
-            if from_ is None or to is None:
-                print("Invalid move...\n")
-                continue
-            if isinstance(self.board[from_], Empty):
-                print("Not a piece...\n")
-                continue
-            if self.board[from_] is None:
-                print("Not on the board...\n")
-                continue
-            if to not in self.board[from_].moves():
-                print("Invalid move...\n")
-                continue
-            if self.board[from_].color != self.board.current_color:
-                print('Not this players turn...\n')
-                continue
-
-            self.move(from_, to)
-            self.current_color = self.opponent_color()
+if __name__ == '__main__':
+    logging.basicConfig(
+        filename='game.log',
+        filemode='w',
+        level=logging.INFO
+    )
+    agent = Agent()
+    game = Game()
+    game.play(agent)
