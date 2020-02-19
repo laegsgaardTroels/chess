c1f7e64c165c6ce33aef953bb71f3350f7d47fb4
Troels Laegsgaard <laegsgaardtroels@gmail.com>
2020-02-19 21:13:55 +0100

    Added 10 random games as unit test.

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ A simple implementation of chess
 - [ ] Agent should only search promising paths.
 - [ ] Agent should search a fixed number of nodes as opposed to being limited by depth.
 - [ ] Add support for castling.
-- [ ] Create a test for random errors. E.g. play 50 random games and test if any errors are raised (should not be the case).
+- [x] Create a test for random errors. E.g. play 50 random games and test if any errors are raised (should not be the case).
---
+++ tests/test_simulations.py
@@ -0,0 +1,19 @@
+import pytest
+
+from chess import Game
+from random import choice
+
+SIMULATIONS = 10
+
+
+@pytest.mark.parametrize("idx", range(SIMULATIONS))
+def test_simulation(idx):
+    """Test that a simulation doesn't fail."""
+    game = Game()
+    while not (
+        game.is_checkmate(game.current_color)
+        or game.is_draw()
+        or game.is_check(game.opponent_color(game.current_color))
+    ):
+        move = choice(game.moves(game.current_color))
+        game.move(*move)
