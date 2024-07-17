f329ab8bf2eccbbd06a28449ef368a9e6c9f1dd4
Troels Laegsgaard <laegsgaardtroels@gmail.com>
2024-07-17 14:52:16 +0200

    Adds tests of en passant

---
+++ tests/test_environment_en_passant.py
@@ -0,0 +1,87 @@
+import pytest
+from chess._constants import BLACK, WHITE
+from chess._environment import ActionFlag
+from chess._utils import (
+    state_init,
+    actions_str,
+)
+
+A3 = 1 << 40
+B6 = 1 << 17
+NO_EN_PASSANT = 0
+
+
+@pytest.mark.parametrize(
+    "color,board,en_passant_square_black,en_passant_square_white,n_en_passant_actions",
+    [
+        (
+            WHITE,
+            "♜♞♝♛♚♝♞♜"
+            "♟ ♟♟♟♟♟♟"
+            "        "
+            "♙♟      "
+            "        "
+            "        "
+            " ♙♙♙♙♙♙♙"
+            "♖♘♗♕♔♗♘♖",
+            NO_EN_PASSANT,
+            B6,
+            1
+        ),
+        (
+            WHITE,
+            "♜♞♝♛♚♝♞♜"
+            "♟ ♟♟♟♟♟♟"
+            "        "
+            "♙♟♙     "
+            "        "
+            "        "
+            " ♙ ♙♙♙♙♙"
+            "♖♘♗♕♔♗♘♖",
+            NO_EN_PASSANT,
+            B6,
+            2
+        ),
+        (
+            BLACK,
+            "♜♞♝♛♚♝♞♜"
+            "♟ ♟♟♟♟♟♟"
+            "        "
+            "        "
+            "♙♟      "
+            "        "
+            " ♙♙♙♙♙♙♙"
+            "♖♘♗♕♔♗♘♖",
+            A3,
+            NO_EN_PASSANT,
+            1
+        ),
+    ],
+)
+def test_en_passant(
+    env,
+    color,
+    board,
+    en_passant_square_black,
+    en_passant_square_white,
+    n_en_passant_actions,
+):
+    state = state_init(
+        color=color,
+        board=board,
+        en_passant_square_black=en_passant_square_black,
+        en_passant_square_white=en_passant_square_white,
+    )
+    actions = env.actions(state)
+    if en_passant_square_black:
+        assert (
+            sum(actions["action_flag"] ==
+                ActionFlag.move_black_pawn_en_passant)
+            == n_en_passant_actions
+        ), f"\n{actions_str(actions)}"
+    else:
+        assert (
+            sum(actions["action_flag"] ==
+                ActionFlag.move_white_pawn_en_passant)
+            == n_en_passant_actions
+        ), f"\n{actions_str(actions)}"
